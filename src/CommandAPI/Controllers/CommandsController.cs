8df5c1b25422b2d8d5e0ccb20ad0262119c0afbf
Muh Hafis Tafsani <hafistafsani915uh@gmail.com>
2021-03-07 16:56:29 +0800

    intial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic; // Support IEnumerable
+using Microsoft.AspNetCore.Mvc; 
+namespace CommandAPI.Controllers
+{
+    [Route("tasks")]
+    [ApiController]
+    public class CommandsController : ControllerBase // Inherit from controller base
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> get()
+        {
+            return new string[] {"this","is","hard", "coded"};
+        }
+    }
+}
