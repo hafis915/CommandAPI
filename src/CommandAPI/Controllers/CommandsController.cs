c137a81238ecee7ab560423fb426304335f9217c
Muh Hafis Tafsani <hafistafsani915uh@gmail.com>
2021-03-08 09:22:54 +0800

    DBContext

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,15 +1,45 @@
 using System.Collections.Generic; // Support IEnumerable
 using Microsoft.AspNetCore.Mvc; 
+using CommandAPI.Data;
+using CommandAPI.Models;
+
+
 namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase // Inherit from controller base
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> get()
+        private readonly ICommandAPIRepo _repository ;
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] {"this","is","hard", "coded"};
+            _repository = repository;
+        }
+        // Before DI
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> get()
+        // {
+        //     return new string[] {"this","is","hard", "coded"};
+        // }
+
+        // DI 
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id) 
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if(commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+
         }
     }
 }
